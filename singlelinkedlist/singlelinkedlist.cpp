0298d8b8b36c2470c4b7c80fc7ff4b0131c6d4ac
taznimt <taznimanwar06@gmail.com>
2024-04-25 15:47:44 +0700

    membuat bool searchNode

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -48,6 +48,29 @@ void addNode() {
 	
 
 }
+
+bool searchNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 int main()
 {
    
